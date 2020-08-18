230d88dcff5564f0895a3f8c8d95cc9d879065bf
Ewald Hew <ewaldhew@gmail.com>
2020-08-19 00:00:47 +0800

    Refactor resource view creation

--- include/utils/DirectXHelper.h
+++ include/utils/DirectXHelper.h
@@ -4,6 +4,39 @@
 #include "Helpers/DirectXRaytracingHelper.h"
 
 namespace DirectX {
+    struct ResourceView
+    {
+        UINT heapIndex = UINT_MAX;
+        D3D12_CPU_DESCRIPTOR_HANDLE cpuHandle;
+        D3D12_GPU_DESCRIPTOR_HANDLE gpuHandle;
+    };
+
+    struct OutputResourceView
+    {
+        ComPtr<ID3D12Resource> Resource;
+        ResourceView Srv;
+        union
+        {
+            ResourceView Uav;
+            ResourceView Rtv;
+            ResourceView Dsv;
+        };
+
+        OutputResourceView() : Uav(ResourceView()) {};
+    };
+
+    inline void clearUav(ID3D12GraphicsCommandList* commandList, OutputResourceView uav)
+    {
+        const UINT clear[4] = { 0 };
+        commandList->ClearUnorderedAccessViewUint(uav.Uav.gpuHandle, uav.Uav.cpuHandle, uav.Resource.Get(), clear, 0, nullptr);
+    }
+
+    inline void clearRtv(ID3D12GraphicsCommandList* commandList, OutputResourceView rtv)
+    {
+        const float clearZero[4] = { 0.0f, 0.0f, 0.0f, 0.0f };
+        commandList->ClearRenderTargetView(rtv.Rtv.cpuHandle, clearZero, 0, nullptr);
+    }
+
     inline void AllocateDepthTexture(ID3D12Device* pDevice, UINT64 textureWidth, UINT64 textureHeight, ID3D12Resource **ppResource, D3D12_RESOURCE_STATES initialResourceState = D3D12_RESOURCE_STATE_COMMON, const wchar_t* resourceName = nullptr, std::array<FLOAT, 4> clearValue = {})
     {
         auto texDesc = CD3DX12_RESOURCE_DESC::Tex2D(DXGI_FORMAT_R32_TYPELESS, textureWidth, static_cast<UINT>(textureHeight), 1, 1, 1, 0, D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL);
